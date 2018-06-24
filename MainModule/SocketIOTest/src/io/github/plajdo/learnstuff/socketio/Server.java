7617f368e62bba650d302e8a050d3c7ac9f5c215
Plajdo <31418257+Plajdo@users.noreply.github.com>
2018-06-24 02:18:13 +0200

    push

--- MainModule/SocketIOTest/src/io/github/plajdo/learnstuff/socketio/Server.java
+++ MainModule/SocketIOTest/src/io/github/plajdo/learnstuff/socketio/Server.java
@@ -1,34 +1,88 @@
 package io.github.plajdo.learnstuff.socketio;
 
-import io.socket.client.IO;
-import io.socket.client.Socket;
-import io.socket.emitter.Emitter;
-
-import javax.swing.*;
-import java.net.URISyntaxException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
 public class Server{
 	
-	Server(){
+	private static ServerSocket serverSocket;
+	private static ArrayList<Socket> allClients = new ArrayList<>();
+	
+	static{
 		try{
-			start();
-		}catch(Exception e){
-			e.printStackTrace();
+			serverSocket = new ServerSocket(4000);
+		}catch(IOException e){
+			System.out.println(e.getMessage());
 		}
 		
 	}
 	
-	private void start() throws URISyntaxException{
-		IO.Options options = new IO.Options();
-		options.hostname = "0.0.0.0";
-		options.port = 4000;
+	public static void main(String[] args){
+		do{
+			try{
+				Socket clientSocket = serverSocket.accept();
+				allClients.add(clientSocket);
+				new Thread(() -> {
+					System.out.println("Client connected: " + clientSocket.getInetAddress().getHostName());
+					System.out.println("Connected clients: " + allClients.toString());
+					allClients.remove(new Client(clientSocket).dealWithIt());
+					System.out.println("Client disconnected: " + clientSocket.getInetAddress().getHostName());
+					System.out.println("Connected clients: " + allClients.toString());
+				}).start();
+			}catch(IOException e){
+				e.getMessage();
+			}
+			
+		}while(!allClients.isEmpty());
 		
-		Socket s = IO.socket(options);
+	}
+	
+}
+
+class Client{
+	
+	PrintWriter out;
+	BufferedReader in;
+	Socket clientSocket;
+	
+	public Client(Socket s){
+		clientSocket = s;
+		try{
+			out = new PrintWriter(clientSocket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		}catch(IOException e){
+			System.out.println(e.getMessage());
+		}
 		
-		s.on(Socket.EVENT_MESSAGE, objects -> {
-			JOptionPane.showMessageDialog(null, objects[1]);
-			s.send("a reply yes");
-		});
+	}
+	
+	public Socket dealWithIt(){
+		try{
+			String inputLine;
+			String outputLine;
+			
+			outputLine = "Succesfully connected";
+			out.println(outputLine);
+			
+			while((inputLine = in.readLine()) != null){
+				System.out.println("inputLine = " + inputLine);
+				outputLine = inputLine;
+				out.println(outputLine);
+				if(inputLine.equals("kys")){
+					break;
+				}
+				
+			}
+			
+		}catch(IOException e){
+			System.out.println(e.getMessage());
+		}
+		return clientSocket;
 		
 	}
 	
--- MainModule/SocketIOTest/src/io/github/plajdo/learnstuff/socketio/User.java
+++ MainModule/SocketIOTest/src/io/github/plajdo/learnstuff/socketio/User.java
@@ -1,111 +1,34 @@
 package io.github.plajdo.learnstuff.socketio;
 
-import com.googlecode.lanterna.TerminalPosition;
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.TextColor;
-import com.googlecode.lanterna.gui2.BasicWindow;
-import com.googlecode.lanterna.gui2.Button;
-import com.googlecode.lanterna.gui2.DefaultWindowManager;
-import com.googlecode.lanterna.gui2.EmptySpace;
-import com.googlecode.lanterna.gui2.GridLayout;
-import com.googlecode.lanterna.gui2.Label;
-import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
-import com.googlecode.lanterna.gui2.Panel;
-import com.googlecode.lanterna.gui2.TextBox;
-import com.googlecode.lanterna.gui2.Window;
-import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
-import io.socket.client.IO;
-import io.socket.client.Socket;
-import io.socket.emitter.Emitter;
-
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.Collections;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class User{
 	
-	private static Terminal terminal;
-	private static Screen screen;
-	private static MultiWindowTextGUI gui;
-	
-	private static BasicWindow loginWindow;
-	private static BasicWindow chatWindow;
-	
-	private static Socket socket;
-	
-	public static void main(String[] args) throws IOException{
-		
-		new Server();
-		
-		terminal = new DefaultTerminalFactory().createTerminal();
-		screen = new TerminalScreen(terminal);
-		screen.startScreen();
-		
-		Panel chatPanel = new Panel();
-		chatPanel.addComponent(new Label("kys"));
-		
-		chatWindow = new BasicWindow("Chat");
-		chatWindow.setHints(Collections.singletonList(Window.Hint.CENTERED));
-		chatWindow.setComponent(chatPanel);
-		
-		
-		/*
-		 * Login window
-		 */
-		loginWindow = new BasicWindow("Login");
-		loginWindow.setHints(Collections.singletonList(Window.Hint.FIXED_POSITION));
-		loginWindow.setPosition(TerminalPosition.OFFSET_1x1);
-		
-		Panel loginPanel = new Panel();
-		loginPanel.setLayoutManager(new GridLayout(2));
-		
-		TextBox ip = new TextBox(new TerminalSize(16, 1));
-		TextBox username = new TextBox(new TerminalSize(16, 1));
-		
-		loginPanel.addComponent(new Label("IP: "));
-		loginPanel.addComponent(ip);
-		
-		loginPanel.addComponent(new Label("Name: "));
-		loginPanel.addComponent(username);
-		
-		loginPanel.addComponent(new EmptySpace(new TerminalSize(0, 0)));
-		loginPanel.addComponent(new Button("Connect", User::login));
-		
-		loginWindow.setComponent(loginPanel);
-		
-		gui = new MultiWindowTextGUI(screen, new DefaultWindowManager(), new EmptySpace(TextColor.ANSI.BLUE));
-		gui.addWindowAndWait(loginWindow);
-	
-	}
-	
-	private static void login(){
-		loginWindow.close();
-		
-		try{
-			socket = IO.socket("localhost:4000");
-		}catch(URISyntaxException e){
-			System.err.println(e.getMessage());
+	public static void main(String[] args){
+		try(Socket echoSocket = new Socket("localhost", 4000);
+			PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))){
+			
+			String userInput;
+			System.out.println("Server: " + in.readLine());
+			
+			while((userInput = stdIn.readLine()) != null){
+				out.println(userInput);
+				if(userInput.equals("kys")){
+					break;
+				}
+				System.out.println("Server: " + in.readLine());
+				
+			}
+			
+		}catch(IOException e){
+			System.out.println(e.getMessage());
 		}
-		
-		
-		socket.on(Socket.EVENT_CONNECT, new Emitter.Listener(){
-			@Override
-			public void call(Object... objects){
-				System.out.println("Client: Connected");
-				gui.addWindowAndWait(chatWindow);
-			}
-		}).on(Socket.EVENT_MESSAGE, new Emitter.Listener(){
-			@Override
-			public void call(Object... objects){
-				System.out.println("Client: " + objects[0]);
-			}
-		});
-		
-		socket.connect();
-		socket.send("hyyyeeeyyey");
 		
 	}
 	
